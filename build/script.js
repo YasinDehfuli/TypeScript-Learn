f5fa6b69dff6a0b9163bdd06e8ce566add3456fd
YasinDehfuli <ydehfuli@gmail.com>
2025-03-27 20:51:31 +0330

    chore(typescript): added varibale type of undefined null and void types

--- build/script.js
+++ build/script.js
@@ -5,3 +5,15 @@ const virtual = false;
 const array = [22, 'string', true];
 const arrayNumber = [22, 33, 44];
 const tuple = [22, 'string'];
+const numBoolean = 'SomeThing';
+const nullable = nullReturner();
+const undefinedVariable = undefinedReturner();
+function logger(message) {
+    console.log(message);
+}
+function nullReturner() {
+    return null;
+}
+function undefinedReturner() {
+    return undefined;
+}
--- src/script.ts
+++ src/script.ts
@@ -5,3 +5,19 @@ const virtual: boolean = false;
 const array: any[] = [22, 'string', true]
 const arrayNumber: number[] = [22, 33, 44]
 const tuple: [number, string] = [22, 'string']
+
+const numBoolean: string | boolean = 'SomeThing'
+const nullable: null = nullReturner()
+const undefinedVariable: undefined = undefinedReturner()
+
+function logger(message: string): void {
+    console.log(message);
+}
+
+function nullReturner() {
+    return null
+}
+
+function undefinedReturner() {
+    return undefined
+}
