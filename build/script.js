4d960a14d15d778e8da16f3c544f76c11f9535db
YasinDehfuli <ydehfuli@gmail.com>
2025-03-27 20:27:11 +0330

    chore(typescript): added varibale type of any, and tuple typing & array of some types

--- build/script.js
+++ build/script.js
@@ -2,3 +2,6 @@
 const fullName = "John Doe";
 const num = 5;
 const virtual = false;
+const array = [22, 'string', true];
+const arrayNumber = [22, 33, 44];
+const tuple = [22, 'string'];
--- src/script.ts
+++ src/script.ts
@@ -1,4 +1,7 @@
 const fullName: string = "John Doe";
-const num : number = 5;
-const virtual : boolean = false;
+const num: number = 5;
+const virtual: boolean = false;
 
+const array: any[] = [22, 'string', true]
+const arrayNumber: number[] = [22, 33, 44]
+const tuple: [number, string] = [22, 'string']
