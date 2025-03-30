acae41e2bee76a46a5c35f5158b6a5717e65d78a
YasinDehfuli <ydehfuli@gmail.com>
2025-03-30 17:10:14 +0330

    chore(interface-in-classes): used interface as type for classes

--- build/interface.js
+++ build/interface.js
@@ -10,3 +10,27 @@ let human = { name: 'John', email: 'john@gmail.com' };
 let humanWithPhone = { name: 'John', email: 'john@gmail.com', phone: 1234455 };
 person(human);
 person(humanWithPhone);
+class David {
+    constructor(name, family, age) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+    }
+    nameLogger() {
+        console.log(this.name);
+    }
+}
+class Jason {
+    constructor(name, family, age, phone) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+        this.phone = phone;
+    }
+    phoneLogger() {
+        console.log(this.phone);
+    }
+}
+const jasonDetails = new Jason('jason', 'alba', 28, 993322);
+console.log(jasonDetails);
+jasonDetails.phoneLogger();
--- src/interface.ts
+++ src/interface.ts
@@ -23,3 +23,49 @@ let humanWithPhone = {name: 'John', email: 'john@gmail.com', phone: 1234455};
 
 person(human)
 person(humanWithPhone)
+
+
+// Interface in class 🔻
+
+interface Person {
+    name: string;
+    family: string
+    age: number;
+}
+
+class David implements Person {
+    name: string;
+    family: string;
+    age: number;
+
+    constructor(name: string, family: string, age: number) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+    }
+
+    nameLogger(){
+        console.log(this.name)
+    }
+}
+
+class Jason implements Person {
+    name: string;
+    family: string;
+    age: number;
+    phone: number;
+    constructor(name: string, family: string, age: number, phone : number) {
+        this.name = name;
+        this.family = family;
+        this.age = age;
+        this.phone = phone;
+    }
+
+    phoneLogger(){
+        console.log(this.phone)
+    }
+}
+
+const jasonDetails = new Jason('jason', 'alba', 28, 993322)
+console.log(jasonDetails)
+jasonDetails.phoneLogger()
