feb14cb4d9212c273682daaa5e244f1339805a81
YasinDehfuli <ydehfuli@gmail.com>
2025-03-30 18:19:17 +0330

    chore(generics): use two generic types for class or functions available

--- build/generics-in-class.js
+++ build/generics-in-class.js
@@ -1,12 +1,13 @@
 "use strict";
 class MyList {
-    constructor(data) {
+    constructor(data, data2) {
         this.data = data;
+        this.data2 = data2;
     }
     doAddItem(item) {
         this.data.push(item);
     }
 }
-let myNumberList = new MyList([1, 2, 3]);
+let myNumberList = new MyList([1, 2, 3], [true, false]);
 myNumberList.doAddItem(4);
 console.log(myNumberList.data);
--- src/generics-in-class.ts
+++ src/generics-in-class.ts
@@ -1,11 +1,12 @@
-class MyList<T> {
-    constructor(public data: T[]) {}
+class MyList<T, U> {
+    constructor(public data: T[], private data2: U[]) {
+    }
 
     doAddItem(item: T) {
         this.data.push(item);
     }
 }
 
-let myNumberList: MyList<number> = new MyList<number>([1, 2, 3]);
+let myNumberList: MyList<number, boolean> = new MyList<number, boolean>([1, 2, 3], [true, false]);
 myNumberList.doAddItem(4);
 console.log(myNumberList.data);
